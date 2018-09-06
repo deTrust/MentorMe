dd94d6db211479cd2520a07e54acc512d90388ad
unknown <nhlamulo.trust@gmail.com>
2018-09-06 15:24:21 +0200

    mentor me

---
+++ app/src/main/java/com/example/ntalo/mentorme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ntalo.mentorme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
